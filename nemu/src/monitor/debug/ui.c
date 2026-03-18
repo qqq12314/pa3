5ca9919f5d47edc6af6b04308b68cd5c866f899f
tracer-ics2017 <tracer@njuics.org>
2026-03-18 09:00:05 +0800

    >  compile 161220000 ics Linux qqqics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  09:00:05 up 32 min,  1 user,  load average: 0.05, 0.06, 0.01 aa2f535d4cb02b5220c3d7b736bb2c6ea4890fbb

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -26,9 +26,16 @@ char* rl_gets() {
 
   return line_read;
 }
-
 static int cmd_c(char *args) {
   cpu_exec(-1);
+  return 0;
+}
+static int cmd_si(char *args) {
+  int n = 1;
+  if (args != NULL) {
+    n = atoi(args);
+  }
+  cpu_exec(n);
   return 0;
 }
 
@@ -46,7 +53,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  {"si", "Step one or more instructions", cmd_si},
   /* TODO: Add more commands */
 
 };
