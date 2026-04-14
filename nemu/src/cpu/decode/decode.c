1cfb89a2f0df7d39c361a9b06b3f5c026ce898da
tracer-ics2017 <tracer@njuics.org>
2026-04-14 20:26:40 +0800

    >  compile 161220000 ics Linux qqqics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:26:40 up  2:06,  1 user,  load average: 0.01, 0.04, 0.00 c1540eb3e6a82b935a82e2c5b5712f8fce397fc2

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -32,13 +32,7 @@ static inline make_DopHelper(SI) {
 
   op->type = OP_TYPE_IMM;
 
-  /* TODO: Use instr_fetch() to read `op->width' bytes of memory
-   * pointed by `eip'. Interpret the result as a signed immediate,
-   * and assign it to op->simm.
-   *
-   op->simm = ???
-   */
-  TODO();
+  op->simm = instr_fetch(eip, op->width);
 
   rtl_li(&op->val, op->simm);
 
