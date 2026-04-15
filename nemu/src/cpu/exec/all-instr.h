911784059affff05ac8c1b4513a874861baab8ff
tracer-ics2017 <tracer@njuics.org>
2026-04-15 13:10:12 +0800

    >  compile 161220000 ics Linux qqqics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  13:10:12 up  2:46,  1 user,  load average: 0.16, 0.09, 0.02 d18ac2e5855ab891ef3cefb2b03affe1d1cf2a3a

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -14,3 +14,18 @@ make_EHelper(jmp_rm);
 
 make_EHelper(call);
 make_EHelper(push);
+make_EHelper(add);
+
+make_EHelper(or);
+
+make_EHelper(adc);
+
+make_EHelper(sbb);
+
+make_EHelper(and);
+
+make_EHelper(sub);
+
+make_EHelper(xor);
+
+make_EHelper(cmp);
