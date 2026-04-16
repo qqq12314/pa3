6856dbb7f692167bede6f7c8ce9b2a5f37f0c03a
tracer-ics2017 <tracer@njuics.org>
2026-04-16 20:05:39 +0800

    >  compile 161220000 ics Linux qqqics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  20:05:39 up 13 min,  1 user,  load average: 0.15, 0.06, 0.01 79821b92a8e4e99b1554aa85e6ca4bdc8c2ffeb3

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -2,64 +2,94 @@
 
 void diff_test_skip_qemu();
 void diff_test_skip_nemu();
-
+void raise_intr(uint8_t NO, vaddr_t ret_addr);
 make_EHelper(lidt) {
-  TODO();
+
+  cpu.idtr.limit = vaddr_read(id_dest->addr, 2);
+
+  cpu.idtr.base = vaddr_read(id_dest->addr + 2, 4);
 
   print_asm_template1(lidt);
+
 }
-
-make_EHelper(mov_r2cr) {
-  TODO();
-
-  print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
-}
-
 make_EHelper(mov_cr2r) {
-  TODO();
+
+  switch (id_src->reg) {
+
+    case 0: rtl_li(&t0, cpu.cr0.val); break;
+
+    case 3: rtl_li(&t0, cpu.cr3.val); break;
+    default: panic("unsupported control register");
+  }
+  operand_write(id_dest, &t0);
 
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
 #ifdef DIFF_TEST
+
   diff_test_skip_qemu();
+
 #endif
+
 }
 
 make_EHelper(int) {
-  TODO();
+  raise_intr(id_dest->val, decoding.seq_eip);
 
   print_asm("int %s", id_dest->str);
 
 #ifdef DIFF_TEST
+
   diff_test_skip_nemu();
+
 #endif
+
 }
 
 make_EHelper(iret) {
-  TODO();
+
+  rtl_pop(&decoding.jmp_eip);
+
+  rtl_pop(&t0);
+
+  cpu.eflags.val = t0;
+
+  decoding.is_jmp = 1;
 
   print_asm("iret");
+
 }
 
 uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-  TODO();
-
+  rtl_li(&t0, pio_read(id_src->val, id_dest->width));
+  operand_write(id_dest, &t0);
   print_asm_template2(in);
 
 #ifdef DIFF_TEST
+
   diff_test_skip_qemu();
+
 #endif
+
 }
 
 make_EHelper(out) {
-  TODO();
+
+  pio_write(id_dest->val, id_src->width, id_src->val);
+
+
 
   print_asm_template2(out);
 
+
+
 #ifdef DIFF_TEST
+
   diff_test_skip_qemu();
+
 #endif
+
 }
