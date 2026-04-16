bf6c0b1c3a0c5d250d02bd8ed9e88724cd635dd5
tracer-ics2017 <tracer@njuics.org>
2026-04-16 21:01:24 +0800

    >  compile 161220000 ics Linux qqqics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  21:01:24 up  1:09,  1 user,  load average: 0.16, 0.04, 0.01 32782d184a60cf513a57a51b659ec352d87c2ae

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -63,8 +63,10 @@ make_group(gp4,
 
   /* 0xff */
 make_group(gp5,
-    EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, EMPTY, EMPTY, EMPTY)
+
+    EMPTY, EMPTY, EX(call_rm), EMPTY,
+
+    EX(jmp_rm), EMPTY, EX(push), EMPTY)
 
   /* 0x0f 0x01*/
 make_group(gp7,
