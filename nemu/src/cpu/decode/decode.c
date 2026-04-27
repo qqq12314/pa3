20d45a631942a44551d59683f44ebd7404c56a7a
tracer-ics2017 <tracer@njuics.org>
2026-04-27 13:23:45 +0800

    >  compile 161220000 ics Linux qqqics 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  13:23:45 up 15 min,  1 user,  load average: 0.04, 0.14, 0.09 bc54336471b616e344286ba6a2888e5533c26bb4

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -266,7 +266,7 @@ make_DHelper(a2O) {
 make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
-  decoding.jmp_eip = id_dest->simm + *eip;
+  decoding.jmp_eip = *eip + id_dest->simm;
 }
 
 make_DHelper(push_SI) {
